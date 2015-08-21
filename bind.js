25731431ad01c24c35d2e87d20c2e065c87c7572
Garrett Cox <garrettjcox@gmail.com>
2015-08-21 15:45:21 -0700

    Cleanup bind file & Rename a few things

--- bind.js
+++ bind.js
@@ -8,163 +8,215 @@ Element.prototype.on = function(event, callback) {
   return this;
 }
 
-$('.configButtonContainer').on('click', function() {
-  $('.config').classList.toggle('open');
-  $('.configButtonContainer').classList.toggle('open');
+var $body = $('body');
+var $canvas = $('#canvas');
+var $configB = $('#configButton');
+var $configM = $('#configMenu');
+var $stateB = $('#stateButton');
+var $resetB = $('#resetButton');
+var $speedS = $('#speedSlider');
+var $widthMB = $('#widthMinusButton');
+var $widthI = $('#widthInput');
+var $widthAB = $('#widthAddButton');
+var $heightMB = $('#heightMinusButton');
+var $heightI = $('#heightInput');
+var $heightAB = $('#heightAddButton');
+var $cellSizeMB = $('#cellSizeMinusButton');
+var $cellSizeI = $('#cellSizeInput');
+var $cellSizeAB = $('#cellSizeAddButton');
+var $brushColorI = $('#brushColorInput');
+var $boardColorB = $('#boardColorButton');
+var $opacityB = $('#opacityButton');
+var $colorsB = $('#colorsButton');
+var $mouseB = $('#mouseButton');
+var $saveB = $('#saveButton');
+var $defaultB = $('#defaultButton');
+
+// Config Button
+
+$configB.on('click', function() {
+  $configM.classList.toggle('open');
+  this.classList.toggle('open');
 });
 
-$('#stateButton').on('click', function() {
-  if (this.value === 'Play') {
-    this.value = 'Pause';
-    gol.options.state = 'Play';
-  } else {
-    this.value = 'Play';
-    gol.options.state = 'Pause';
-  }
+// Playback Buttons
+
+$stateB.value = gol.options.state === 'Play' ? 'Pause' : 'Play';
+
+$stateB.on('click', function() {
+  gol.options.state = this.value;
+  this.value = this.value === 'Play' ? 'Pause' : 'Play';
 });
 
-$('#resetButton').on('click', function() {
+$resetB.on('click', function() {
   gol.init();
 });
 
-$('#speed').on('input', function() {
+// Speed Slider
+
+$speedS.value = gol.options.speed;
+
+$speedS.on('input', function() {
   gol.options.speed = this.value;
-}).value = gol.options.speed;
+});
 
-var $width = $('#width');
+// Width Buttons
 
-$width.on('change', function() {
+$widthI.value = gol.options.widthCells;
+
+$widthI.on('input', function() {
   var maxW = Math.floor(window.innerWidth / gol.options.cellSize);
+
   if (this.value > maxW) this.value = maxW;
   gol.options.widthCells = this.value;
   gol.init();
-}).value = gol.options.widthCells;
+});
 
-$('#minusWidth').on('click', function() {
-  gol.options.widthCells = $width.value-= 1;
+$widthMB.on('click', function() {
+  gol.options.widthCells = $widthI.value-= 1;
   gol.init();
 });
 
-$('#addWidth').on('click', function() {
+$widthAB.on('click', function() {
   var maxW = Math.floor(window.innerWidth / gol.options.cellSize);
-  if (+$width.value+1 > maxW) return;
-  gol.options.widthCells = $width.value = (+$width.value + 1);
+  var newW = +$widthI.value + 1;
+
+  if (newW > maxW) return;
+  gol.options.widthCells = $widthI.value = newW;
   gol.init();
 });
 
-var $height = $('#height');
+// Height Buttons
 
-$height.on('change', function() {
+$heightI.value = gol.options.heightCells;
+
+$heightI.on('input', function() {
   var maxH = Math.floor(window.innerHeight / gol.options.cellSize);
+
   if (this.value > maxH) this.value = maxH;
   gol.options.heightCells = this.value;
   gol.init();
-}).value = gol.options.heightCells;
+});
 
-$('#minusHeight').on('click', function() {
-  gol.options.heightCells = $height.value-= 1;
+$heightMB.on('click', function() {
+  gol.options.heightCells = $heightI.value-= 1;
   gol.init();
 });
 
-$('#addHeight').on('click', function() {
+$heightAB.on('click', function() {
   var maxH = Math.floor(window.innerHeight / gol.options.cellSize);
-  if (+$height.value+1 > maxH) return;
-  gol.options.heightCells = $height.value = (+$height.value + 1);
+  var newH = +$heightI.value + 1;
+
+  if (newH > maxH) return;
+  gol.options.heightCells = $heightI.value = newH;
   gol.init();
 });
 
-var $size = $('#cellSize');
+// Cell Size Buttons
 
-$size.on('change', function() {
+$cellSizeI.value = gol.options.cellSize;
+
+$cellSizeI.on('change', function() {
   gol.options.cellSize = this.value;
   var maxW = Math.floor(window.innerWidth / gol.options.cellSize);
   var maxH = Math.floor(window.innerHeight / gol.options.cellSize);
-  if ($width.value > maxW) {
-    $width.value = maxW;
-    gol.options.widthCells = $width.value;
-  }
-  if ($height.value > maxH) {
-    $height.value = maxH;
-    gol.options.heightCells = $height.value;
-  }
-  gol.init();
-}).value = gol.options.cellSize;
 
-$('#minusCellSize').on('click', function() {
-  gol.options.cellSize = $size.value-= 1;
+  if ($widthI.value > maxW) {
+    $widthI.value = maxW;
+    gol.options.widthCells = maxW;
+  }
+  if ($heightI.value > maxH) {
+    $heightI.value = maxH;
+    gol.options.heightCells = maxH;
+  }
   gol.init();
 });
 
-$('#addCellSize').on('click', function() {
-  gol.options.cellSize = $size.value = (+$size.value + 1);
+$cellSizeMB.on('click', function() {
+  gol.options.cellSize = $cellSizeI.value-= 1;
+  gol.init();
+});
+
+$cellSizeAB.on('click', function() {
+  gol.options.cellSize = $cellSizeI.value = (+$cellSizeI.value + 1);
   var maxW = Math.floor(window.innerWidth / gol.options.cellSize);
   var maxH = Math.floor(window.innerHeight / gol.options.cellSize);
-  if ($width.value > maxW) {
-    $width.value = maxW;
-    gol.options.widthCells = $width.value;
+
+  if ($widthI.value > maxW) {
+    $widthI.value = maxW;
+    gol.options.widthCells = maxW;
   }
-  if ($height.value > maxH) {
-    $height.value = maxH;
-    gol.options.heightCells = $height.value;
+  if ($heightI.value > maxH) {
+    $heightI.value = maxH;
+    gol.options.heightCells = maxH;
   }
   gol.init();
 });
 
-$('#cellColor').on('change', function() {
+// Brush/Cell Color Input
+
+$brushColorI.value = gol.options.cellColor;
+
+$brushColorI.on('input', function() {
   gol.options.brush = hexToRgb(this.value);
   gol.render();
-}).value = gol.options.cellColor;
+});
 
-$('#opacity').on('click', function() {
+// Board Color Button
+
+$boardColorB.value = gol.options.boardColor === 'Dark Mode' ? 'Light Mode' : 'Dark Mode';
+if (gol.options.boardColor === 'Dark Mode') $body.classList.add('dark');
+
+$boardColorB.on('click', function() {
+  gol.options.boardColor = this.value;
+  this.value = this.value === 'Dark Mode' ? 'Light Mode' : 'Dark Mode';
+  $body.classList.toggle('dark');
+  gol.render();
+});
+
+// Opacity Button
+
+if (gol.options.opacity) $opacityB.classList.add('active');
+
+$opacityB.on('click', function() {
   gol.options.opacity = !gol.options.opacity;
-  gol.render();
   this.classList.toggle('active');
+  gol.render();
 });
 
-if (gol.options.opacity) {
-  $('#opacity').classList.toggle('active');
-}
+// Colors Button
 
-$('#colors').on('click', function() {
+if (gol.options.inheritColors) $colorsB.classList.add('active');
+$('#colorTitle').innerText = gol.options.inheritColors ? 'Brush Color' : 'Cell Color';
+
+$colorsB.on('click', function() {
   gol.options.inheritColors = !gol.options.inheritColors;
-  $('#colorTitle').innerText = $('#colorTitle').innerText === 'Brush Color' ? 'Cell Color' : 'Brush Color';
-  gol.render();
   this.classList.toggle('active');
-});
-
-if (gol.options.inheritColors) {
-  $('#colors').classList.toggle('active');
-  $('#colorTitle').innerText = 'Brush Color';
-}
-
-$('#interactive').on('click', function() {
-  gol.options.interactive = !gol.options.interactive;
-  this.classList.toggle('active');
-  $('#canvas').classList.toggle('pointer');
-});
-
-$('#boardColor').on('click', function() {
-  if (this.value === 'Light Mode') {
-    this.value = 'Dark Mode';
-    $('body').classList.remove('dark');
-    gol.options.boardColor = 'Light Mode';
-  } else {
-    this.value = 'Light Mode';
-    $('body').classList.add('dark');
-    gol.options.boardColor = 'Dark Mode';
-  }
+  $('#colorTitle').innerText = gol.options.inheritColors ? 'Brush Color' : 'Cell Color';
   gol.render();
 });
+
+// Mouse Button
 
 if (gol.options.interactive) {
-  $('#interactive').classList.toggle('active');
-  $('#canvas').classList.toggle('pointer');
+  $mouseB.classList.add('active');
+  $canvas.classList.add('pointer');
 }
 
-$('#save').on('click', function() {
+$mouseB.on('click', function() {
+  gol.options.interactive = !gol.options.interactive;
+  this.classList.toggle('active');
+  $canvas.classList.toggle('pointer');
+});
+
+// Save Button
+
+$saveB.on('click', function() {
   gol.save();
 });
 
-$('#defaults').on('click', function() {
+// Default Button
+
+$defaultB.on('click', function() {
   gol.defaults();
 });
